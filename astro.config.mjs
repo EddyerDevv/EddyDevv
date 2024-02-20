79aab3e4d748cc13c40103dca2fe0579b0b33bd1
EddyerDevv <urbinaramalazo@1986@gmail.com>
2024-02-19 21:38:32 -0600

    Actualización de Astro a la versión 4.4.0 y agregado de Astro Font. Eliminación de archivos innecesarios y ajustes en el diseño.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  output: "server",
+});
