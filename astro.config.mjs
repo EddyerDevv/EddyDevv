8af1ee86b9f67a1dcd711fcc81e43370812b5e84
EddyerDevv <urbinaramalazo1986@gmail.com>
2024-04-07 02:54:29 -0600

    delete: deleted adapter vercel and output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
-  output: "server",
-  adapter: vercel(),
 });
