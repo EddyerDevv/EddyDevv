5792a868e249c16712a40ce709116df05c1ee501
EddyerDevv <urbinaramalazo1986@gmail.com>
2024-04-09 11:01:32 -0600

    fix: fix name props in svg

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,9 +14,9 @@ function AboutMe() {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="icon"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
